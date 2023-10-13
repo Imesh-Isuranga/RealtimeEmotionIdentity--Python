a27ad811ea08c9637ee0e7b9687b8eda9428c07b
Imesh-Isuranga <imeshisuranga00@gmail.com>
2023-10-13 13:14:34 +0530

    Created python program for faces detection for given image

--- image_face_detection.py
+++ image_face_detection.py
@@ -16,3 +16,16 @@ all_faces_locations = face_recognition.face_locations(image_to_detect,model="hog
 
 #printing the number of faces in the array
 print("There are {} face(s) in this images".format(len(all_faces_locations)))
+
+#looping through the face locations
+for index,current_face_location in enumerate(all_faces_locations):
+    #splitting the tuple to et the four position values
+    top_pos,right_pos,bottom_pos,left_pos = current_face_location
+    print('Found face {} at top:{},right:{},bottom:{},left:{}'.format(index+1, top_pos,right_pos,bottom_pos,left_pos))
+    
+    
+    #slice image array by positions inside the loop
+    current_face_image = image_to_detect[top_pos:bottom_pos,left_pos:right_pos]
+    
+    #show each sliced face inside the loop
+    cv2.imshow("Face No: " + str(index+1),current_face_image)
---
+++ realtime_face_detection.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 13 13:12:25 2023
+
+@author: Imesh
+"""
+
